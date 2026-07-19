8b73ccaee03831b7e8f121f671317dcf07350d99
drew-wiseman <got2gofast2fast2quick@gmail.com>
2026-07-19 19:57:55 -0400

    Moved from in-memory list to SQLite + SQLAlchemy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, HTTPException, status
+from database import SessionLocal, engine, Base
+from fastapi import FastAPI, HTTPException, status, Depends
+from models import GameDB
 from pydantic import BaseModel, Field
 from typing import Literal, Optional
+from sqlalchemy.orm import Session
 
 class GameCreate(BaseModel):
     title: str
@@ -17,7 +20,14 @@ class Game(GameCreate):
     id: int
 
 app = FastAPI() # create app instance
-games = [] #in memory list to hold games
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/games")
 def get_games(
@@ -31,58 +41,59 @@ def get_games(
     condition: Optional[Literal["loose", "CIB", "sealed"]] = None,
     max_purchase_price: Optional[float] = None,
     min_purchase_price: Optional[float] = None,
+    db: Session = Depends(get_db)
 ):
-    result = games
+    query = db.query(GameDB)
     if title is not None:
-        result = [g for g in result if g.title == title]
+        query = query.filter(GameDB.title == title)
     if platform is not None:
-        result = [g for g in result if g.platform == platform]
+        query = query.filter(GameDB.platform == platform)
     if genre is not None:
-        result = [g for g in result if g.genre == genre]
+        query = query.filter(GameDB.genre == genre)
     if min_curr_price is not None:
-        result = [g for g in result if g.current_market_price >= min_curr_price]
+        query = query.filter(GameDB.current_market_price >= min_curr_price)
     if max_curr_price is not None:
-        result = [g for g in result if g.current_market_price <= max_curr_price]
+        query = query.filter(GameDB.current_market_price <= max_curr_price)
     if start_year is not None:
-        result = [g for g in result if g.year_released >= start_year]
+        query = query.filter(GameDB.year_released >= start_year)
     if end_year is not None:
-        result = [g for g in result if g.year_released <= end_year]
+        query = query.filter(GameDB.year_released <= end_year)
     if condition is not None:
-        result = [g for g in result if g.condition == condition]
+        query = query.filter(GameDB.condition == condition)
     if max_purchase_price is not None:
-        result = [g for g in result if g.purchase_price <= max_purchase_price]
+        query = query.filter(GameDB.purchase_price <= max_purchase_price)
     if min_purchase_price is not None:
-        result = [g for g in result if g.purchase_price >= min_purchase_price]
-    return result
+        query = query.filter(GameDB.purchase_price >= min_purchase_price)
+    return query.all()
 
 @app.post("/games")
-def create_game(game: GameCreate):
+def create_game(game: GameCreate, db: Session = Depends(get_db)):
     game_data = game.model_dump()
-    g = Game(id=len(games) + 1, **game_data)
-    games.append(g)
+    g = GameDB(**game_data)
+    db.add(g)
+    db.commit()
+    db.refresh(g)
     return g
 
 @app.get("/games/{id}")
-def get_game(id: int):
-    for g in games:
-        if g.id == id:
-            return g
-    raise HTTPException (
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Game ID '{id}' not found."
-    )
-
-@app.delete("/games/{id}")
-def del_game(id: int):
-    target = -1
-    for i, g in enumerate(games):
-        if g.id == id:
-            target = i
-            break
-    if target == -1:
+def get_game(id: int, db: Session = Depends(get_db)):
+    result = db.query(GameDB).filter(GameDB.id == id).first()
+    if result is None:
         raise HTTPException (
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Game ID '{id}' not found."
         )
-    else:
-        games.pop(target)
+    return result
+
+
+@app.delete("/games/{id}")
+def del_game(id: int, db: Session = Depends(get_db)):
+    result = db.query(GameDB).filter(GameDB.id == id).first()
+    if result is None:
+        raise HTTPException (
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Game ID '{id}' not found."
+        )
+    db.delete(result)
+    db.commit()
+    return {"message": f"Game ID '{id}' deleted."}
---
+++ models.py
@@ -0,0 +1,14 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Float, Boolean
+
+class GameDB(Base):
+    __tablename__ = "games"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    platform = Column(String, nullable=False)
+    genre = Column(String, nullable=False)
+    year_released = Column(Integer, nullable=False)
+    current_market_price = Column(Float, nullable=False)
+    played = Column(Boolean, nullable=False)
+    condition = Column(String, nullable=True)
+    purchase_price = Column(Float, nullable=True)
